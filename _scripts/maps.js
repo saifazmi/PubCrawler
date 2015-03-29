f7c059dbeaf0a96da76f517c5ea2f18117051912
Matt Dean <mxd434@cs.bham.ac.uk>
2015-03-29 09:46:48 +0100

    working with (yes/no/dontcare), still needs styling but changing div bg colour will require changing js as well

--- _scripts/maps.js
+++ _scripts/maps.js
@@ -1,7 +1,16 @@
 var barObjs = [];   
 var latlon = "";
 
-alert = function() {};
+alert = function() {}
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
 // ---------------- REQUEST DATA -----------------
 function requestData(url, callBack)
 {
@@ -34,23 +43,70 @@ function loadBars(jsonhttp){
     
  
 }
-
+/*
 function showBars(objects){
     document.getElementById("panel-body").innerHTML = "";
-    var $list = $("<ul>");
+    var $wrapper = $("<div>");
     
     for(var i = 0; i < objects.length; i++){
-        var $li = $("<li>");
-        var $bar = $("<input>", {type: "checkbox", checked: "true"});
+        var $div = $("<div>");
+        var $bar = $("<button>", {class: "btn btn-success", value:"yes"});
+        $bar.click = function() { $div.css("background-color", "green"); };
+        $div.append($bar);
+        var $bar = $("<button>", {class: "btn btn-primary", value:"dontcare"});
+        $div.append($bar);
+        var $bar = $("<button>", {class: "btn btn-danger", value:"no"});
+        $div.append($bar);
+        $div.append(objects[i].name);
         
-        $li.append($bar);
-        $li.append(objects[i].name);
-        
-        $list.append($li);
+        $wrapper.append($div);
     }
-    console.log($list);
-    $(".panel-body").append($list);
-}   
+    console.log($wrapper);
+    $(".panel-body").append($wrapper);
+}  
+*/
+function showBars(objects){
+    document.getElementById("panel-body").innerHTML = "";
+    var wrapper = document.createElement("div");
+        wrapper.id = "objectwrapper";
+    
+    for(var i=0; i<objects.length; i++){
+        var div = document.createElement("div");
+            div.style.backgroundColor = "lightblue";
+        var btn = document.createElement("button");
+            btn.className = "btn btn-success";
+            btn.value = "yes";
+            btn.onclick = (function() { 
+                var div1 = div;
+                return function(){
+                div1.style.backgroundColor = "green";}; })();
+        div.appendChild(btn);
+        
+        var btn = document.createElement("button");
+            btn.className = "btn btn-primary";
+            btn.value = "yes";
+            btn.onclick = (function() { 
+                var div1 = div;
+                return function(){
+                div1.style.backgroundColor = "lightblue";}; })();
+        div.appendChild(btn);
+        
+        var btn = document.createElement("button");
+            btn.className = "btn btn-danger";
+            btn.value = "yes";
+            btn.onclick = (function() { 
+                var div1 = div;
+                return function(){
+                div1.style.backgroundColor = "red";}; })();
+        div.appendChild(btn);
+        
+        
+        div.appendChild(document.createTextNode(objects[i].name));
+        wrapper.appendChild(div);
+    }
+    document.getElementById("panel-body").appendChild(wrapper);
+    
+}
 
 function initialise(){
     getLocation();  
@@ -73,10 +129,61 @@ function storePosition(position) {
 //get bar objects
 function getBarObjs(){
     var temp = barObjs.slice(0, barObjs.length);
-    temp.length = document.getElementById("numberpubs").value;
     return temp;   
 }
 
+function sortObjs(){
+    var temp = getBarObjs();
+    console.log(temp);
+     var numberpubs = document.getElementById("numberpubs").value;
+    
+    var objectwrapper = document.getElementById("objectwrapper");
+    var objectdivs = objectwrapper.childNodes;
+    for(var i=0; i<objectdivs.length; i++){
+        switch(objectdivs[i].style.backgroundColor){
+            case "lightblue":
+                temp[i].option = "dontcare";
+                break;
+            case "red":
+                temp[i].option = "no";
+                break;
+            case "green":
+                temp[i].option = "yes";
+                break;
+            default:
+                temp[i].option = "dontcare";
+        }
+    }
+    //get all yes's
+    var yes = temp.filter(filterObjYes);
+    //make up the numbers with dont care if needed
+    if (Object.size(yes) < numberpubs){
+     var difference = numberpubs - Object.size(yes);
+        var temp = barObjs.slice(0, barObjs.length);
+        var dontcare = temp.filter(filterObjDC);
+        for(var i=0; i<difference; i++){
+            yes.push(dontcare[i]);
+        }
+    }
+    console.log(yes);
+    return yes; 
+    
+}
+
+function filterObjYes(value){
+    if(value.option == "yes"){
+        return true;
+    }
+    else{ return false;}
+    
+}
+function filterObjDC(value){
+    if(value.option == "dontcare"){
+        return true;
+    }
+    else{ return false;}
+    
+}
 
      if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getMapCenter);
@@ -161,18 +268,31 @@ function esricode() {
                         
         //button click event listeners can't be added directly in HTML when the code is wrapped in an AMD callback
   
-       on(dom.byId("numberpubs"), "click", function() { clearRoutes();clearStops();document.getElementById("panel-body").innerHTML = ""; });   
-      on(dom.byId("findCrawlBtn"), "click", function(){ addStops();solveRoute();});
+       on(dom.byId("numberpubs"), "click", function() {    
+            clearRoutes();
+            clearStops();
+            document.getElementById("panel-body").innerHTML = "";
+           removeEventHandlers();
+            var temp = getBarObjs();
+            showBars(temp); 
+       });   
+      on(dom.byId("findCrawlBtn"), "click", function(){
+          clearStops();
+          addStops();
+          solveRoute();
+            
+      
+      });
 
         //Begins listening for click events to add stops
         function addStops() {
           removeEventHandlers();
-            var temp = getBarObjs();
+            var temp = sortObjs();
             for(var i=0; i<temp.length; i++){ 
                 var obj = temp[i];
                 mapOnClick_addStops_connect = addOurStop(obj.longitude, obj.latitude);
             }
-              showBars(temp); 
+              
         }
 
         //Clears all stops
